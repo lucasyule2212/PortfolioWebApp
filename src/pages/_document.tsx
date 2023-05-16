c99425eb3c215f8ffe014391086b1111f78d5be4
Lucas Yule <lyrma@cin.ufpe.br>
2023-05-16 16:28:27 -0300

    refactor(_document): updating the body background color to use discord theme

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,7 +16,7 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
         </Head>
-        <body className="bg-zinc-800">
+        <body className="bg-discord-gray-4">
           <Main />
           <NextScript />
         </body>
