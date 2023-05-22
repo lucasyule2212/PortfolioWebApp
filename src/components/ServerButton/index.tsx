8ef8950d4ed73a5084b3c6a0e11c4556c32f88ee
Lucas Yule <lyrma@cin.ufpe.br>
2023-05-22 12:05:04 -0300

    feat(SidebarServer): adding ServerButton component
    adding ServerButton component + updating SidebarServers component + logo icons + framer-motion
    animated component

---
+++ src/components/ServerButton/index.tsx
@@ -0,0 +1,77 @@
+import Image from 'next/image';
+import React, { useMemo, useState } from 'react';
+import { motion } from 'framer-motion';
+// import { Container } from './styles';
+
+type ServerButtonProps = {
+  src: string;
+  alt: string;
+  isTopHeader?: boolean;
+};
+
+const ServerButton: React.FC<ServerButtonProps> = ({ src, alt, isTopHeader }: ServerButtonProps) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isSelected, setIsSelected] = useState(false);
+
+  const variants = useMemo(
+    () => ({
+      default: {
+        height: 0,
+      },
+      hover: {
+        height: isSelected ? '80%' : '40%',
+        backgroundColor: '#fafafa',
+        transition: {
+          duration: 0.2,
+        },
+      },
+      selected: {
+        height: '80%',
+        backgroundColor: '#fafafa',
+        transition: {
+          duration: 0.1,
+        },
+      },
+    }),
+    [isSelected]
+  );
+
+  return (
+    <div className={`flex items-center ${isTopHeader ? 'mt-1' : ''}`}>
+      <motion.div
+        className={`w-1 rounded-r-lg mr-1 ${isSelected ? 'bg-white' : ''}`}
+        variants={variants}
+        initial="default"
+        animate={isHovered ? 'hover' : isSelected ? 'selected' : 'default'}
+        transition={{ duration: 0.2 }}
+      />
+
+      <button
+        className={`flex items-center justify-center w-12 h-12 rounded-full  bg-discord-gray-3 text-white transition-all duration-200 ${
+          isHovered || isSelected ? 'rounded-2xl hover:bg-discord-blue' : ''
+        }`}
+        style={{
+          borderRadius: isHovered ? '1rem' : '100%',
+        }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        onClick={() => setIsSelected(!isSelected)}
+      >
+        <Image
+          className={`flex items-center justify-center w-12 h-12 rounded-full  bg-discord-gray-3 text-white transition-all duration-200 ${
+            isHovered ? 'rounded-2xl hover:bg-discord-blue' : ''
+          }`}
+          style={{
+            borderRadius: isHovered ? '1rem' : '100%',
+          }}
+          src={`/assets/images/${src}`}
+          alt={alt}
+          width={500}
+          height={500}
+        />
+      </button>
+    </div>
+  );
+};
+
+export default ServerButton;
--- src/layouts/SidebarServers/index.tsx
+++ src/layouts/SidebarServers/index.tsx
@@ -1,9 +1,17 @@
+import ServerButton from '@/components/ServerButton';
 import React from 'react';
 
 // import { Container } from './styles';
 
 const SidebarServers: React.FC = () => {
-  return <div className="flex flex-col w-18 bg-discord-gray-6"></div>;
+  return (
+    <div className="flex flex-col w-18 bg-discord-gray-6 p-2 pl-0 items-center gap-2">
+      <ServerButton src="yuleHeadSmile.png" alt="sticker yule smile" isTopHeader />
+      {/* customized divider */}
+      <div className="w-8 h-[2px] bg-discord-gray-3 rounded-md ml-2"></div>
+      <ServerButton src="logoYule.png" alt="logo yule" />
+    </div>
+  );
 };
 
 export default SidebarServers;
