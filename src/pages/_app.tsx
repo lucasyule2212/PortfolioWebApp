c2a308b8756b93a0ee990e279be3ea65f645f71f
Lucas Yule <lucas.rocha@fabricadenegocio.com.br>
2023-06-07 00:08:56 -0300

    feat(global): add shadcn ui lib base configs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import MainLayout from '@/layouts/Main';
-import '../styles/global.css';
+import '../styles/globals.css';
 
 import type { AppProps } from 'next/app';
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ['class'],
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    container: {
+      center: true,
+      // padding: '2rem',
+      // screens: {
+      //   '2xl': '1400px',
+      // },
+    },
     fontFamily: {
       sans: ['Poppins', 'sans-serif'],
     },
-    textColor: {
-      primary: '#fafafa',
+    fontWeight: {
+      thin: 200,
+      normal: 400,
+      semibold: 500,
+      bold: 700,
     },
     fontSize: {
       xs: '0.75rem',
@@ -21,6 +32,9 @@ module.exports = {
       '6xl': '4rem',
     },
     extend: {
+      textColor: {
+        primary: '#fafafa',
+      },
       width: {
         18: '4.7rem',
       },
@@ -29,6 +43,8 @@ module.exports = {
       },
       backgroundColor: {
         discord: {
+          'light-blue': '#949cf7',
+          'medium-blue': '#4752c4',
           blue: '#5865f2',
           'gray-1': '#424549',
           'gray-2': '#36393e',
@@ -36,20 +52,57 @@ module.exports = {
           'gray-4': '#2b2d31',
           'gray-5': '#202225',
           'gray-6': '#1e1f22',
+          'gray-7': '#111214',
         },
       },
       boxShadow: {
         bottom: '0px 2px 1px 0px rgba(0,0,0,0.25)',
       },
       colors: {
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
         discord: {
           blue: '#5865f2',
+          'medium-blue': '#4752c4',
+          'light-blue': '#949cf7',
           'gray-1': '#424549',
           'gray-2': '#36393e',
           'gray-3': '#313338',
           'gray-4': '#2b2d31',
           'gray-5': '#202225',
           'gray-6': '#1e1f22',
+          'gray-7': '#111214',
           'green-1': '#37ff77',
           'green-2': '#287e29',
         },
@@ -76,7 +129,26 @@ module.exports = {
           900: '#2a4365',
         },
       },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 };
