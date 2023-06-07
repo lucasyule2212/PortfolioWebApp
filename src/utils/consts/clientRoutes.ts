fdcd38023f10a63e45748932b94d08f35d6895e7
Lucas Yule <lucas.rocha@fabricadenegocio.com.br>
2023-06-07 00:06:53 -0300

    refactor(clientRoutes): updating clientRoutes return typing

--- src/utils/consts/clientRoutes.ts
+++ src/utils/consts/clientRoutes.ts
@@ -1,6 +1,11 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 
-const clientRoutes: { [key: string]: any } = {
+const clientRoutes: {
+  [key: string]: {
+    title: string;
+    description: string;
+  };
+} = {
   '/': {
     title: 'Lucas Yule | Portfolio Web App',
     description: 'Portfolio Web Application of Lucas Yule built with Next.js and TypeScript',
