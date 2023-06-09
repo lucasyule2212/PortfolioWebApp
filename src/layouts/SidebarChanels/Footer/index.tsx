83529cd80305379a02fbc48a5fdebcda09846c86
Lucas Yule <lucas.rocha@fabricadenegocio.com.br>
2023-06-08 22:57:24 -0300

    refactor(SidebarChannelsFooter): adding margin auto to Footer component

--- src/layouts/SidebarChanels/Footer/index.tsx
+++ src/layouts/SidebarChanels/Footer/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 // import { Container } from './styles';
 
 const Footer: React.FC = () => {
-  return <footer className="bg-discord-gray-5 h-14"></footer>;
+  return <footer className="bg-discord-gray-5 h-14 mt-auto"> test</footer>;
 };
 
 export default Footer;
