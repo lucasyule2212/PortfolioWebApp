959a1aeac5460d6df23b1f1c3c30f708a9b1d1f3
Lucas Yule <lucas.rocha@fabricadenegocio.com.br>
2023-06-29 01:08:15 -0300

    feat(ChannelUserSidebarUserCard): adding base component to user card

---
+++ src/components/ChannelUserSidebarUserCard/index.tsx
@@ -0,0 +1,55 @@
+import { cn } from '@/lib/utils';
+import { cva } from 'class-variance-authority';
+import React, { useMemo, useState } from 'react';
+import RoundedUserImage from '../RoundedUserImage';
+import { FaCrown } from 'react-icons/fa';
+
+// import { Container } from './styles';
+
+interface IChannelUserSidebarUserCardProps {
+  label: string;
+  isServerOwner?: boolean;
+}
+
+const ChannelUserSidebarUserCard: React.FC<IChannelUserSidebarUserCardProps> = ({
+  label,
+  isServerOwner = false,
+}: IChannelUserSidebarUserCardProps) => {
+  const styles = cva('flex gap-2 p-1 items-center rounded-md cursor-pointer', {
+    variants: {
+      variant: {
+        default: '',
+        hover: 'bg-discord-gray-2 text-primary',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  });
+
+  const [isHovered, setIsHovered] = useState(false);
+
+  const variant = useMemo(() => {
+    if (isHovered) {
+      return 'hover';
+    }
+
+    return 'default';
+  }, [isHovered]);
+
+  return (
+    <div
+      className={cn(styles({ variant }))}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <RoundedUserImage />
+      <div className="flex items-center gap-1">
+        <span className={`text-xs ${isHovered ? 'text-primary' : 'text-discord-gray-0'} font-semibold`}>{label}</span>
+        {isServerOwner && <FaCrown className="text-yellow-500 text-xs" />}
+      </div>
+    </div>
+  );
+};
+
+export default ChannelUserSidebarUserCard;
--- src/layouts/ChanelComponent/ChanelUsersSidebar/index.tsx
+++ src/layouts/ChanelComponent/ChanelUsersSidebar/index.tsx
@@ -1,9 +1,18 @@
+import ChannelUserSidebarUserCard from '@/components/ChannelUserSidebarUserCard';
 import React from 'react';
 
 // import { Container } from './styles';
 
 const ChanelUsersSidebar: React.FC = () => {
-  return <div className=" bg-discord-gray-4">LALALALALL</div>;
+  return (
+    <div className=" bg-discord-gray-4 pt-6 px-4 flex flex-col gap-1">
+      <div className="flex items-center gap-2">
+        <span className="font-semibold text-discord-gray-0 text-xs">DISPONÍVEL - </span>
+        <span className="text-discord-gray-0 text-xs">2</span>
+      </div>
+      <ChannelUserSidebarUserCard label="Lucas Yule" isServerOwner={true} />
+    </div>
+  );
 };
 
 export default ChanelUsersSidebar;
