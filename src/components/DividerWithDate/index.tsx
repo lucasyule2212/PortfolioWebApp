6dcf8f0ee61a6b6854700d1ca830fe692e601b30
Lucas Yule <lucas.rocha@fabricadenegocio.com.br>
2023-07-28 17:51:54 -0300

    refactor(DividerWithDate): updating DividerWithDate component locale + min-width config

--- src/components/DividerWithDate/index.tsx
+++ src/components/DividerWithDate/index.tsx
@@ -1,15 +1,18 @@
 import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import React from 'react';
 
 // import { Container } from './styles';
 
 const DividerWithDate: React.FC = () => {
-  const actualDate = format(new Date(), 'dd MMMM yyyy');
+  const actualDate = format(new Date(), 'dd MMMM yyyy', {
+    locale: ptBR,
+  });
 
   return (
     <div className="flex gap-1 w-full items-center justify-center">
       <div className="w-full h-px my-8 bg-discord-gray-1 border-0 dark:bg-gray-700" />
-      <span className="text-discord-gray-0 text-xs w-44 ml-2 p-0 font-semibold">{actualDate}</span>
+      <span className="text-discord-gray-0 text-xs min-w-48 w-48 ml-2 p-0 font-semibold">{actualDate}</span>
       <div className="w-full h-px my-8 bg-discord-gray-1 border-0 dark:bg-gray-700" />
     </div>
   );
