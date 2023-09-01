001b499a093174a963e63bfffb8a54110f9eea83
Lucas Yule <lyrma@cin.ufpe.br>
2023-09-01 11:29:37 -0300

    feat(GuestProfile+RoundedUserImage): updating GuestProfile and RoundedUserImage components
    adding the information from guestUser to reflect the componets relative to it

--- src/components/ChannelsFooter/GuestProfile/index.tsx
+++ src/components/ChannelsFooter/GuestProfile/index.tsx
@@ -1,15 +1,68 @@
 import RoundedUserImage from '@/components/RoundedUserImage';
-import React from 'react';
+import { useGuestUser } from '@/store/GuestUser';
+import { motion } from 'framer-motion';
+import React, { useState } from 'react';
 
 // import { Container } from './styles';
 
 const GuestProfile: React.FC = () => {
+  const [isHovered, setIsHovered] = useState(false);
+  const { guestUser } = useGuestUser();
+  const fastTransition = {
+    type: 'tween',
+    duration: 0.2, // Adjust the duration as needed
+  };
+
+  const slowTransition = {
+    type: 'tween',
+    duration: 0.2, // Adjust the duration as needed
+  };
+
+  const slideTopVariants = {
+    initial: { y: '0%' },
+    hover: { y: '-50%' },
+  };
+
+  const slideBottomVariants = {
+    initial: { y: '50%' },
+    hover: { y: '0%' },
+  };
+
   return (
-    <div className={`flex w-[50%] hover:bg-[#4e5058]/60  rounded-sm py-1 hover:cursor-pointer`}>
-      <RoundedUserImage />
+    <div
+      className={`flex w-[50%] hover:bg-[#4e5058]/60  rounded-sm py-1 hover:cursor-pointer`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => {
+        setIsHovered(false);
+      }}
+    >
+      <div className="relative">
+        <RoundedUserImage isGuest />
+        <div className="absolute bottom-0 right-0 w-4 h-4 bg-green-600 rounded-full border-[3px] border-discord-gray-5"></div>
+      </div>
       <div className="flex flex-col justify-center ml-2">
-        <span className="text-xs text-primary font-semibold">usuario</span>
-        <span className="text-[0.7rem] text-discord-gray-0">usuario#22</span>
+        <span className="text-xs text-primary font-semibold">{guestUser.name}</span>
+
+        <motion.span
+          className="text-[0.7rem] text-discord-gray-0"
+          initial="initial"
+          animate={isHovered ? 'hover' : 'initial'}
+          variants={slideTopVariants}
+          transition={fastTransition}
+        >
+          {isHovered ? '' : 'Disponível'}
+        </motion.span>
+        {isHovered && (
+          <motion.span
+            className="text-[0.7rem] text-discord-gray-0"
+            initial="initial"
+            animate="hover"
+            variants={slideBottomVariants}
+            transition={slowTransition}
+          >
+            {guestUser.username}
+          </motion.span>
+        )}
       </div>
     </div>
   );
--- src/components/RoundedUserImage/index.tsx
+++ src/components/RoundedUserImage/index.tsx
@@ -1,11 +1,32 @@
+import { useGuestUser } from '@/store/GuestUser';
+import Avatar from 'boring-avatars';
 import React from 'react';
+import Image from 'next/image';
 
 // import { Container } from './styles';
-
-const RoundedUserImage: React.FC = () => {
+type RoundedUserImageProps = {
+  isGuest?: boolean;
+};
+const RoundedUserImage: React.FC<RoundedUserImageProps> = ({ isGuest }) => {
+  const { guestUser } = useGuestUser();
   return (
-    <div className="flex items-center justify-center w-8 h-8 bg-red-500 rounded-full">
-      <span className="text-2xl text-discord-white">?</span>
+    <div className="flex items-center justify-center w-8 h-8 rounded-full">
+      {isGuest ? (
+        <Avatar
+          variant="beam"
+          size="50px"
+          name={guestUser.name !== '' ? guestUser.name : 'Discord'}
+          colors={['#30182B', '#F0F1BC', '#60F0C0', '#FF360E', '#191F04']}
+        />
+      ) : (
+        <Image
+          className="scale-150"
+          src={`/assets/images/yuleHeadSmile.png`}
+          alt={'sticker yule smile'}
+          width={200}
+          height={200}
+        />
+      )}
     </div>
   );
 };
