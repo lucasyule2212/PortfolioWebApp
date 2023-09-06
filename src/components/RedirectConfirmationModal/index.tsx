a08033844359fd917def6429af05e47bd4bcbe54
Lucas Yule <lyrma@cin.ufpe.br>
2023-09-06 15:51:02 -0300

    feat(RedirectConfirmationModal): adding RedirectConfirmationModal translations + layout adjustment

--- src/components/RedirectConfirmationModal/index.tsx
+++ src/components/RedirectConfirmationModal/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogFooter } from '../ui/dialog';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogFooter,
+  DialogOverlay,
+} from '../ui/dialog';
 import { Button } from '../ui/button';
+import { useTranslation } from 'react-i18next';
 
 // import { Container } from './styles';
 interface RedirectConfirmationModalProps {
@@ -14,22 +23,23 @@ const RedirectConfirmationModal: React.FC<RedirectConfirmationModalProps> = ({
   setIsOpen,
   redirectLink,
 }: RedirectConfirmationModalProps) => {
+  const { t } = useTranslation();
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
       {/* <DialogTrigger>Open</DialogTrigger> */}
-      {/* <DialogOverlay /> */}
+      <DialogOverlay className="backdrop-blur-none bg-black/60" />
       <DialogContent className="bg-discord-gray-6 border-discord-gray-7">
         <DialogHeader>
-          <DialogTitle className="text-center text-discord-gray-0">Você será redirecionado:</DialogTitle>
+          <DialogTitle className="text-center text-discord-gray-0">{t('redirectConfirmationModal_title')}</DialogTitle>
         </DialogHeader>
         <DialogDescription className="text-discord-gray-0 text-base">
-          Ao confirmar, uma nova aba será aberta em:
+          {t('redirectConfirmationModal_subtitle')}
           <h4 className="text-discord-medium-blue mb-4 font-semibold">{redirectLink}</h4>
-          Deseja continuar?
+          <strong>{t('redirectConfirmationModal_subtitle_2')}</strong>
         </DialogDescription>
         <DialogFooter>
           <Button variant="destructive" onClick={() => setIsOpen(false)}>
-            <p>Cancelar</p>
+            <p>{t('button_cancel')}</p>
           </Button>
           <Button
             variant="confirm"
@@ -41,7 +51,7 @@ const RedirectConfirmationModal: React.FC<RedirectConfirmationModalProps> = ({
               setIsOpen(false);
             }}
           >
-            <p>Confirmar</p>
+            <p>{t('button_confirm')}</p>
           </Button>
         </DialogFooter>
       </DialogContent>
