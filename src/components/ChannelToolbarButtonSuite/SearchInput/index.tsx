10ba0cec268ede20ae338399e116b848370f7156
Lucas Yule <lyrma@cin.ufpe.br>
2023-09-06 15:26:40 -0300

    feat(SearchInput): adding SearchInput translation

--- src/components/ChannelToolbarButtonSuite/SearchInput/index.tsx
+++ src/components/ChannelToolbarButtonSuite/SearchInput/index.tsx
@@ -1,10 +1,12 @@
 import { SearchInput as Input } from '@/components/ui/searchInput';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 // import { Container } from './styles';
 
 const SearchInput: React.FC = () => {
-  return <Input placeholder="Buscar" className="outline-none w-32 focus:w-48 transition-all" />;
+  const { t } = useTranslation();
+  return <Input placeholder={t('searchInput_placeholder')} className="outline-none w-32 focus:w-48 transition-all" />;
 };
 
 export default SearchInput;
