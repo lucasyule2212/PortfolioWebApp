80d09cc06096073d52efd1a611dfb3cb79e47e1d
Lucas Yule <lyrma@cin.ufpe.br>
2023-09-06 12:26:27 -0300

    refactor(SidebarChannels): adding channels routing function + title translation

--- src/layouts/SidebarChanels/index.tsx
+++ src/layouts/SidebarChanels/index.tsx
@@ -4,15 +4,30 @@ import Footer from './Footer';
 import ChannelGroupAccordion from '@/components/ChannelGroupAccordion';
 import TextChannelButton from '@/components/TextChannelButton';
 import AudioChannelButton from '@/components/AudioChannelButton';
+import { useTranslation } from 'react-i18next';
+import { useRouter } from 'next/router';
 
 // import { Container } from './styles';
 
 const SidebarChanels: React.FC = () => {
+  const { t } = useTranslation();
+  const router = useRouter();
+  const handleChannelClick = (route: string) => {
+    router.push(route);
+  };
   return (
     <div className="flex flex-col h-full w-64 bg-discord-gray-4">
       <Header />
-      <ChannelGroupAccordion title="CANAIS DE TEXTO">
-        <TextChannelButton title="Projetos" />
+      <ChannelGroupAccordion title={t('sidebarChannels_textChannel1')}>
+        <TextChannelButton title={t('channel_home')} onClick={() => handleChannelClick('/home')} />
+        <TextChannelButton title={t('channel_about_me')} onClick={() => handleChannelClick('/about')} />
+        <TextChannelButton title={t('channel_tech_skills')} onClick={() => handleChannelClick('/tech_skills')} />
+        <TextChannelButton
+          title={t('channel_professional_xp')}
+          onClick={() => handleChannelClick('/professional_experience')}
+        />
+        <TextChannelButton title={t('channel_personal_projects')} onClick={() => handleChannelClick('/projects')} />
+        <TextChannelButton title={t('channel_coding_config')} onClick={() => handleChannelClick('/setup')} />
         <AudioChannelButton title="Lo-fi bot" />
       </ChannelGroupAccordion>
       <Footer />
