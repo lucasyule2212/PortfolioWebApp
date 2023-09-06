5677eb1e512a3f42a40a52b3cad140860cca437c
Lucas Yule <lyrma@cin.ufpe.br>
2023-09-06 13:04:57 -0300

    refactor(SidebarChannels): refactoring onClick call + adding route parameter to TextChannelButton

--- src/components/TextChannelButton/index.tsx
+++ src/components/TextChannelButton/index.tsx
@@ -6,17 +6,18 @@ import { ExternalLink } from 'lucide-react';
 // import { Container } from './styles';
 type TextChannelButtonProps = {
   title: string;
-  onClick: () => void;
+  handleClick: (route: string) => void;
+  route: string;
 };
 
-const TextChannelButton: React.FC<TextChannelButtonProps> = ({ title, onClick }: TextChannelButtonProps) => {
+const TextChannelButton: React.FC<TextChannelButtonProps> = ({ title, handleClick, route }: TextChannelButtonProps) => {
   const [isHovered, setIsHovered] = useState(false);
   return (
     <Button
       className="flex px-2 bg-transparent hover:bg-discord-gray-2 w-full py-0 justify-start items-center h-8  gap-2 rounded-sm"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
-      onClick={onClick}
+      onClick={() => handleClick(route)}
     >
       <BiHash className="text-discord-gray-0" size={20} />
       <span
--- src/layouts/SidebarChanels/index.tsx
+++ src/layouts/SidebarChanels/index.tsx
@@ -19,15 +19,20 @@ const SidebarChanels: React.FC = () => {
     <div className="flex flex-col h-full w-64 bg-discord-gray-4">
       <Header />
       <ChannelGroupAccordion title={t('sidebarChannels_textChannel1')}>
-        <TextChannelButton title={t('channel_home')} onClick={() => handleChannelClick('/home')} />
-        <TextChannelButton title={t('channel_about_me')} onClick={() => handleChannelClick('/about')} />
-        <TextChannelButton title={t('channel_tech_skills')} onClick={() => handleChannelClick('/tech_skills')} />
+        <TextChannelButton title={t('channel_home')} handleClick={handleChannelClick} route={'/home'} />
+        <TextChannelButton title={t('channel_about_me')} handleClick={handleChannelClick} route={'/about'} />
+        <TextChannelButton title={t('channel_tech_skills')} handleClick={handleChannelClick} route={'/tech_skills'} />
         <TextChannelButton
           title={t('channel_professional_xp')}
-          onClick={() => handleChannelClick('/professional_experience')}
+          handleClick={handleChannelClick}
+          route={'/professional_experience'}
         />
-        <TextChannelButton title={t('channel_personal_projects')} onClick={() => handleChannelClick('/projects')} />
-        <TextChannelButton title={t('channel_coding_config')} onClick={() => handleChannelClick('/setup')} />
+        <TextChannelButton
+          title={t('channel_personal_projects')}
+          handleClick={handleChannelClick}
+          route={'/projects'}
+        />
+        <TextChannelButton title={t('channel_coding_config')} handleClick={handleChannelClick} route={'/setup'} />
         <AudioChannelButton title="Lo-fi bot" />
       </ChannelGroupAccordion>
       <Footer />
