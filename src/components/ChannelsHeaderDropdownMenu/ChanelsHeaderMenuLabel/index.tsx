e48419467f723e03d14ab5b23aab0f39fed9b642
Lucas Yule <lyrma@cin.ufpe.br>
2023-09-26 00:08:15 -0300

    refactor(ChannelsHeaderMenuLabel): adding translation to Quick Access text

--- src/components/ChannelsHeaderDropdownMenu/ChanelsHeaderMenuLabel/index.tsx
+++ src/components/ChannelsHeaderDropdownMenu/ChanelsHeaderMenuLabel/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import LightningIcon from './LigthningIcon';
 import { DropdownMenuLabel } from '@/components/ui/dropdown-menu';
+import { useTranslation } from 'react-i18next';
 
 // import { Container } from './styles';
 
 const ChanelsHeaderMenuLabel: React.FC = () => {
+  const { t } = useTranslation();
   return (
     <DropdownMenuLabel className="flex gap-2 items-center">
-      Quick Access <LightningIcon />
+      {t('quick_access')} <LightningIcon />
     </DropdownMenuLabel>
   );
 };
