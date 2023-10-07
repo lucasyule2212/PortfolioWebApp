9bf60a989ed10f11f3b3efd2b34dafcd6807fed0
Lucas Yule <lyrma@cin.ufpe.br>
2023-10-07 20:07:47 -0300

    feat(projects-page): adding message mapping + static props with messages data

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,17 +1,58 @@
 import ChannelHeader from '@/components/ChannelHeader';
+import MessageComponent from '@/components/MessageComponent';
+import MessageAreaContainer from '@/layouts/ChannelComponent/MessageAreaContainer';
 import MainLayout from '@/layouts/Main';
+import { Message, fetchHygraphQuery } from '@/utils/fetchHygraphQuery';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 // import { Container } from './styles';
+const getPageData = () => {
+  const query = `
+  query ProjectsQuery {
+    messages(where: {slug_contains: "projects"}) {
+      localizations(locales: [en, pt_BR], includeCurrent: true) {
+        content {
+          html
+        }
+      }
+      reactions
+      id
+    }
+  }
+  `;
+  return fetchHygraphQuery({ query });
+};
 
-const Projects: React.FC = () => {
+const Projects = ({ messages }: { messages: Message[] }) => {
+  const { i18n } = useTranslation();
   return (
     <MainLayout>
-      <main>
+      <MessageAreaContainer>
         <ChannelHeader />
-      </main>
+        {messages &&
+          messages.map(message => (
+            <MessageComponent
+              key={message.id}
+              username={'lucas_yule'}
+              user_image_url=""
+              content={message.content[i18n.language] || ''}
+              reactions={message.reactions}
+            />
+          ))}
+      </MessageAreaContainer>
     </MainLayout>
   );
 };
+
+export async function getStaticProps() {
+  const messages = await getPageData();
+
+  return {
+    props: {
+      messages,
+    },
+  };
+}
 
 export default Projects;
